4fe0d660f7ceaf606f561bc799e0330b20539a51
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2023-09-28 14:32:52 -0600

    Cambios para el juego de opciones

--- Proyecto_Equipo1/Proyecto_Equipo1/QuestionView.swift
+++ Proyecto_Equipo1/Proyecto_Equipo1/QuestionView.swift
@@ -59,6 +59,6 @@ struct QuestionView: View {
 
 //struct QuestionView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        QuestionView(question: Question(title: <#T##String#>, answer: <#T##String#>, options: <#T##[String]#>))
+//        QuestionView(question: .constant(Question))
 //    }
 //}
