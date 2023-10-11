94c20e5043a593de5946ec5570d53b90cbc06e37
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2023-10-11 12:10:58 -0600

    Agregué archivos para la cámara

---
+++ Proyecto_Equipo1/Proyecto_Equipo1/Data/Classification.swift
@@ -0,0 +1,11 @@
+//
+//  Classification.swift
+//  Proyecto_Equipo1
+//
+//  Created by Alumno on 11/10/23.
+//
+
+struct Classification: Decodable {
+    // TODO: replace with the name of your keys from mydata.json and some default values
+    var label: String = "A"
+}
