3aeb2ebf767ab454ded562c9a401d24f96c76f0d
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2023-10-15 23:26:56 -0600

    Cámara mejorada

--- Proyecto_Equipo1/Proyecto_Equipo1/ShowSignView.swift
+++ Proyecto_Equipo1/Proyecto_Equipo1/ShowSignView.swift
@@ -17,24 +17,24 @@ struct ShowSignView: View {
                     .foregroundColor(.green)
                     .cornerRadius(10)
                     .frame(width: 70, height: 50)
-                    .offset(y: -100)
+                    .offset(y: -50)
                 Image("Camara")
                     .resizable()
                     .frame(width: 40, height: 40)
-                    .offset(y: -100)
+                    .offset(y: -50)
             }
             ZStack{
                 Rectangle()
                     .foregroundColor(.white)
                     .cornerRadius(10)
                     .frame(width: 50, height: 50)
-                    .offset(y: -100)
+                    .offset(y: -50)
                 Text(labelData.label.capitalized)
                     .multilineTextAlignment(.center)
                     .foregroundColor(Color("Text"))
                     .font(Font
                         .custom("Oxygen-Regular", size: 20))
-                    .offset(y: -100)
+                    .offset(y: -50)
             }
         }
     }
