3caf447b077ec6ba015838607870c9ea3af20e2a
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2023-10-17 17:38:27 -0600

    asdfg

--- Proyecto_Equipo1/Proyecto_Equipo1/GameSelect.swift
+++ Proyecto_Equipo1/Proyecto_Equipo1/GameSelect.swift
@@ -90,7 +90,7 @@ struct GameSelect: View {
                 }
                 .padding()
                 .offset(y: -20)
-                NavigationLink(destination: MateSenias()){
+                NavigationLink(destination: QuestionGame()){
                     ZStack{
                         Rectangle()
                             .foregroundColor(Color("GameStack4"))
