5473fa975119d6ce382d6c25cf33ea79414f8034
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2023-10-18 19:30:23 -0600

    Cambios Extensos

--- Proyecto_Equipo1/Proyecto_Equipo1/GameSelect.swift
+++ Proyecto_Equipo1/Proyecto_Equipo1/GameSelect.swift
@@ -90,7 +90,7 @@ struct GameSelect: View {
                 }
                 .padding()
                 .offset(y: -20)
-                NavigationLink(destination: QuestionGame()){
+                NavigationLink(destination: Home()){
                     ZStack{
                         Rectangle()
                             .foregroundColor(Color("GameStack4"))
