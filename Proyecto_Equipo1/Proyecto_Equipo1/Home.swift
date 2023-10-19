b7081a365c34d5360ff6e5792e5759701228bc40
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2023-10-18 21:53:02 -0600

    asdfg

---
+++ Proyecto_Equipo1/Proyecto_Equipo1/Home.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+//first view of the app
+struct Home: View {
+
+    //var for the score
+    @State var score = 0
+    @State var isBlinking = false
+
+    var body: some View {
+        ZStack {
+            Color("Primary")
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                
+                Text("Welcome to the quiz game")
+                    .scaleEffect(isBlinking ? 1.2 : 1)
+                    .opacity(isBlinking ? 0 : 100)
+                    .frame(width: 250, height: 50, alignment: .center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
+                            self.isBlinking.toggle()
+                        }
+                    }
+                
+                //button to start the quiz
+                NavigationLink(destination: QuizView()) {
+                    Text("START QUIZ")
+                        .scaleEffect(isBlinking ? 1.2 : 1)
+                        .opacity(isBlinking ? 0 : 100)
+                        .frame(width: 250, height: 50, alignment: .center)
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                }
+                
+                HStack {
+                    //display your score
+                    NavigationLink(destination: ProgressView()) {
+                        Text("View Progress")
+                            .scaleEffect(isBlinking ? 1.2 : 1)
+                            .opacity(isBlinking ? 0 : 100)
+                            .frame(width: 250, height: 50, alignment: .center)
+                            .background(Color.green)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                }
+
+                }
+            }
+            // VStack
+            .navigationBarTitle("Quiz", displayMode: .inline)
+        }
+        // NavigationView
+        
+    }
+    // BODY
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
